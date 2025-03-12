8c2e5ce87b4f2bfd5412b0cc2b84c4c7e656ae7d
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-03-12 10:09:43 +0000

    install lens from snap since it's not in e.g. dnf, so this is more portable (since we have snapd available_

--- 3-lens.sh
+++ 3-lens.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
+# curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
 
-sudo apt update && sudo apt install lens
+# sudo apt update && sudo apt install lens
 
-lens-desktop &
+# can we just do this from snaps?
+# it doesn't seem to be in dnf, though the website has an rpm for download
+sudo snap install kontena-lens --classic
+
+kontena-lens &
 

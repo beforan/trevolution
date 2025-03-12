5c3ce5b0329ed19a998269effc39505eedf41567
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-03-12 10:12:01 +0000

    wip comments around personal hurdles with fedora, zsh, nix

--- 1-install.sh
+++ 1-install.sh
@@ -27,14 +27,16 @@ sudo snap install microk8s --classic --channel="$CHANNEL"
 
 # Add current user to the microk8s group
 echo "Adding current user to the microk8s group..."
-sudo usermod -a -G microk8s $USER
+sudo usermod -a -G microk8s $USER # TODO: on fedora this needs a reboot? can we avoid this? newgrp microk8s? (spawns a new shell)
 mkdir -p ~/.kube
 sudo chown -f -R $USER ~/.kube
 
 
 # Update the local system's PATH variable to include MicroK8s binaries
 echo "Updating the local system's PATH variable..."
-echo 'export PATH=$PATH:$HOME/.snap/bin' >> ~/.bashrc
+
+# TODO: what about other shells? (i have the same zsh problem as the aliases)
+echo 'export PATH=$PATH:/snap/bin' >> ~/.bashrc
 . ~/.bashrc
 
 # Verify installation by checking the version of kubectl installed by MicroK8s
@@ -56,30 +58,32 @@ sudo microk8s enable hostpath-storage
 #Alias
 echo "Creating Alias for KUBECTL"
 
+# TODO: make alias adding conditional
+# I use nix home-manager to build my .zshrc, so you can't just add to it manually
 
 # Detect the user's default shell configuration file
-if [ -n "$ZSH_VERSION" ]; then
-    SHELL_CONFIG="$HOME/.zshrc"
-elif [ -n "$FISH_VERSION" ]; then
-    SHELL_CONFIG="$HOME/.config/fish/config.fish"
-else
-    SHELL_CONFIG="$HOME/.bashrc"
-fi
+# if [ -n "$ZSH_VERSION" ]; then
+#     SHELL_CONFIG="$HOME/.zshrc"
+# elif [ -n "$FISH_VERSION" ]; then
+#     SHELL_CONFIG="$HOME/.config/fish/config.fish"
+# else
+#     SHELL_CONFIG="$HOME/.bashrc"
+# fi
 
-# Add alias if not already present
-if ! grep -q "alias kubectl='microk8s kubectl'" "$SHELL_CONFIG"; then
-    echo "alias kubectl='microk8s kubectl'" >> "$SHELL_CONFIG"
-    echo "Alias added to $SHELL_CONFIG"
-else
-    echo "Alias already exists in $SHELL_CONFIG"
-fi
+# # Add alias if not already present
+# if ! grep -q "alias kubectl='microk8s kubectl'" "$SHELL_CONFIG"; then
+#     echo "alias kubectl='microk8s kubectl'" >> "$SHELL_CONFIG"
+#     echo "Alias added to $SHELL_CONFIG"
+# else
+#     echo "Alias already exists in $SHELL_CONFIG"
+# fi
 
-# Apply changes for the current session
-case "$SHELL_CONFIG" in
-    *bashrc) source "$HOME/.bashrc" ;;
-    *zshrc) source "$HOME/.zshrc" ;;
-    *config.fish) source "$HOME/.config/fish/config.fish" ;;
-esac
+# # Apply changes for the current session
+# case "$SHELL_CONFIG" in
+#     *bashrc) source "$HOME/.bashrc" ;;
+#     *zshrc) source "$HOME/.zshrc" ;;
+#     *config.fish) source "$HOME/.config/fish/config.fish" ;;
+# esac
 
 echo "Alias setup complete. Restart your terminal or run 'source $SHELL_CONFIG' to apply."
 
